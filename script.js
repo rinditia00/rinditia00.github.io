14ec0bde178d997ac43a3d94b22a4005eb1bc1fc
rinditia00 <147014715+rinditia00@users.noreply.github.com>
2023-10-05 09:57:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
-// Function to fetch and update cryptocurrency prices from Binance
-function fetchCryptoPrices() {
-    const coins = ['BTC', 'LTC', 'ETH', 'XRP', 'ADA', 'SUI', 'WLD', 'MATIC'];
-
-    coins.forEach(coin => {
-        const coinElement = document.getElementById(`${coin}-price`);
-        
-        // Make an AJAX request to the Binance public API for coin price
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://api.binance.com/api/v3/ticker/price?symbol=${coin}USDT`, true);
-        
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                const coinPrice = parseFloat(response.price).toFixed(2); // Extract coin price
-                
-                // Update the HTML element with the new price
-                coinElement.textContent = `$${coinPrice}`;
-                
-                // Check if the price went up, down, or stayed the same
-                const prevCoinPrice = parseFloat(coinElement.getAttribute('data-prev-price'));
-                if (!isNaN(prevCoinPrice) && coinPrice > prevCoinPrice) {
-                    // Price went up, blink green
-                    coinElement.style.color = 'green';
-                    setTimeout(() => {
-                        coinElement.style.color = 'black'; // Return to black
-                    }, 1000); // Blink for 1 second
-                } else if (!isNaN(prevCoinPrice) && coinPrice < prevCoinPrice) {
-                    // Price went down, blink red
-                    coinElement.style.color = 'red';
-                    setTimeout(() => {
-                        coinElement.style.color = 'black'; // Return to black
-                    }, 1000); // Blink for 1 second
-                }
-                
-                // Update the data attribute with the new price
-                coinElement.setAttribute('data-prev-price', coinPrice);
+// Function to fetch and update Bitcoin price from Binance
+function fetchBitcoinPrice() {
+    const bitcoinPriceElement = document.getElementById('bitcoin-price');
+    
+    // Make an AJAX request to the Binance public API for Bitcoin price
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT', true);
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const newBitcoinPrice = parseFloat(response.price);
+            
+            // Get the previous Bitcoin price stored in a data attribute
+            const prevBitcoinPrice = parseFloat(bitcoinPriceElement.getAttribute('data-prev-price'));
+            
+            // Update the data attribute with the new price
+            bitcoinPriceElement.setAttribute('data-prev-price', newBitcoinPrice);
+            
+            // Update the HTML element with the new price
+            bitcoinPriceElement.textContent = `$${newBitcoinPrice.toFixed(2)}`;
+            
+            // Check if the price went up, down, or stayed the same
+            if (newBitcoinPrice > prevBitcoinPrice) {
+                // Price went up, blink green
+                bitcoinPriceElement.style.color = 'green';
+                setTimeout(() => {
+                    bitcoinPriceElement.style.color = 'black'; // Return to black
+                }, 1000); // Blink for 1 second
+            } else if (newBitcoinPrice < prevBitcoinPrice) {
+                // Price went down, blink red
+                bitcoinPriceElement.style.color = 'red';
+                setTimeout(() => {
+                    bitcoinPriceElement.style.color = 'black'; // Return to black
+                }, 1000); // Blink for 1 second
             }
-        };
-        
-        xhr.send();
-    });
+        }
+    };
+    
+    xhr.send();
 }
 
-// Fetch cryptocurrency prices from Binance initially
-fetchCryptoPrices();
+// Fetch Bitcoin price from Binance initially
+fetchBitcoinPrice();
 
-// Fetch cryptocurrency prices from Binance every second
-setInterval(fetchCryptoPrices, 1000);
+// Fetch Bitcoin price from Binance every second
+setInterval(fetchBitcoinPrice, 1000);
