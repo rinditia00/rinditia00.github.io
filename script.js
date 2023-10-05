a7be8ee010ac4a1300e1d952c33c862a8af17c56
rinditia00 <147014715+rinditia00@users.noreply.github.com>
2023-10-05 09:15:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Function to fetch and update Bitcoin price from Binance
+function fetchBitcoinPrice() {
+    const bitcoinPriceElement = document.getElementById('bitcoin-price');
+    
+    // Make an AJAX request to the Binance API
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT', true);
+    xhr.setRequestHeader('X-MBX-APIKEY', 'YOUR_API_KEY'); // Replace with your Binance API key
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const bitcoinPrice = parseFloat(response.price).toFixed(2); // Extract Bitcoin price
+            
+            // Update the HTML element with the new price
+            bitcoinPriceElement.textContent = `$${bitcoinPrice}`;
+        }
+    };
+    
+    xhr.send();
+}
+
+// Fetch Bitcoin price from Binance initially
+fetchBitcoinPrice();
+
+// Fetch Bitcoin price from Binance every second
+setInterval(fetchBitcoinPrice, 1000);
